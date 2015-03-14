ca2895fece64a47c972b7809d0c5437cf2b01458
Bart Hoekstra <bart-hoekstra@live.nl>
2015-03-14 20:41:35 +0100

    My Boats added, check if owner function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	has_many :boats
+
 	#validates_presence_of :password, :on => :create
 	#validates_presence_of :password_confirmation
 	#validates_confirmation_of :password
